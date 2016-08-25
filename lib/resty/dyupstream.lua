a0204660c5c713c41db17426ea315c41fe9f676a
shaojieyue <yuesj@huatu.com>
2016-08-25 17:35:39 +0800

    完善代码

--- lib/resty/dyupstream.lua
+++ lib/resty/dyupstream.lua
@@ -5,48 +5,19 @@ local lock = require "resty.lock"
 local ngx_timer_at = ngx.timer.at
 local ngx_log = ngx.log
 local ngx_ERR = ngx.ERR
-local ngx_sleep = ngx.sleep
 local ngx_worker_exiting = ngx.worker.exiting
-
---servers data
-_M.data = {}
---etcd cluster node list
-_M.etcd_node_list = {}
-local function log(c)
-    ngx_log(ngx_ERR, c)
+--etcd host list
+local etcd_node_list = {}
+--all server list exam:{"server-name":{"count":0,"nodes":{}}}
+local servers = {}
+local function log(message)
+    ngx_log(ngx_ERR, message)
 end
 
 string.split = function(s, p)
     local rt = {}
     string.gsub(s, '[^' .. p .. ']+', function(w) table.insert(rt, w) end)
     return rt
-end
-
-local function split_addr(s)
-    host, port = s:match("(.*):([0-9]+)")
-
-    -- verify the port
-    local p = tonumber(port)
-    if p == nil then
-        return "127.0.0.1", 0, "port invalid"
-    elseif p < 1 or p > 65535 then
-        return "127.0.0.1", 0, "port invalid"
-    end
-
-    -- verify the ip addr
-    local chunks = { host:match("(%d+)%.(%d+)%.(%d+)%.(%d+)") }
-    if (#chunks == 4) then
-        for _, v in pairs(chunks) do
-            if (tonumber(v) < 0 or tonumber(v) > 255) then
-                return "127.0.0.1", 0, "host invalid"
-            end
-        end
-    else
-        return "127.0.0.1", 0, "host invalid"
-    end
-
-    -- verify pass
-    return host, port, nil
 end
 
 local function get_lock()
@@ -73,27 +44,26 @@ end
 
 
 local function open_dump_file(mode)
-    local dump_path = _M.conf.dump_path.. "/" .. _M.conf.etcd_path:gsub("/", "_") .. ".json"
-    log("open file:"..dump_path)
+    local dump_path = _M.conf.dump_path .. "/" .. _M.conf.etcd_path:gsub("/", "_") .. ".json"
     local dump_file, err = io.open(dump_path, mode)
     if not io.type(dump_file) then --file not exist
-        os.execute("mkdir -p  ".._M.conf.dump_path)
+        os.execute("mkdir -p  " .. _M.conf.dump_path)
         --touch file
-        os.execute("touch ".. dump_path)
+        os.execute("touch " .. dump_path)
         dump_file, err = io.open(dump_path, mode)
     end
-    return dump_file,err;
+    return dump_file, err;
 end
 
 local function dump_tofile()
     local saved = false
     log("start dump file")
     while not saved do
-        local locks ,err = lock:new(_M.conf.dict)
+        local locks, err = lock:new(_M.conf.dict)
         if err then
-            log("get locks error:"..err)
+            log("get locks error:" .. err)
         end
-        local elapsed,err = locks:lock("dump_file_lock")
+        local elapsed, err = locks:lock("dump_file_lock")
         if not err then
             local file, err = open_dump_file('w')
             if file == nil then
@@ -102,35 +72,31 @@ local function dump_tofile()
                 return false
             end
 
-            local data = cjson_safe.encode(_M.data)
+            local data = cjson_safe.encode(servers)
             file:write(data)
             file:flush()
             file:close()
             saved = true
-            log("dump file success. data:"..data)
+            log("dump file success. data:" .. data)
             locks:unlock()
         else
-            log("wait for lock fail. err:"..err)
+            log("wait for lock fail. err:" .. err)
         end
     end
 end
 
-local function query_etcd_data(url)
-    log("query etcd data ,url="..url)
+local function query_etcd_data(uri)
     local http_conn = http.new()
-    http_conn:set_timeout(1000)
-    for index, node in pairs(_M.etcd_node_list) do
+    http_conn:set_timeout(3000)
+    for index, node in pairs(etcd_node_list) do
         http_conn:connect(node.host, node.port)
-        local res, err = http_conn:request({ path = url, method = "GET" })
-        if not err then
-            local body, err = res:read_body()
-            ngx.log(ngx.ERR, body)
-            if not err then
-                local json_body = cjson_safe.decode(body)
-                --errorCode=100 ->	“Key not found”
-                if json_body then
-                    return json_body
-                end
+        local http_address = "http://"..node.host..":"..node.port..uri
+        local res, err = http_conn:request_uri(http_address,{method = "GET"})
+        if res then
+            local json_body = cjson_safe.decode(res.body)
+            --errorCode=100 ->	“Key not found”
+            if json_body then
+                return json_body
             end
         end
     end
@@ -142,8 +108,7 @@ local function update_server(name)
     --server register url
     local server_register_url = "/v2/keys" .. _M.conf.etcd_path .. name .. "?recursive=true"
     local json_body = query_etcd_data(server_register_url)
-    local servers = {}
-
+    local nodes = {}
     --not exist server or no server node
     if json_body.errorCode or not json_body.node.nodes then
         --do nothing
@@ -153,18 +118,18 @@ local function update_server(name)
             local data, err = cjson_safe.decode(server_node.value)
             if not err then
                 --insert the server info to server list
-                table.insert(servers, { host = data.host, port = data.port, weight = data.weight, current_weight = 0 })
+                table.insert(nodes, { host = data.host, port = data.port, weight = data.weight, current_weight = 0 })
             end
         end
     end
 
-    if not _M.data[name] then --not exist ,init it
-    _M.data[name] = {}
+    if not servers[name] then --not exist ,init it
+        servers[name] = {}
     end
 
-    _M.data[name].servers = servers
-    _M.data[name].count = table.getn(servers)
-    ngx.log(ngx.ERR, "update [" .. name .. "] servers, data=" .. cjson_safe.encode(_M.data[name]))
+    servers[name].nodes = nodes
+    servers[name].count = table.getn(nodes)
+    ngx.log(ngx.ERR, "update [" .. name .. "] nodes, data=" .. cjson_safe.encode(servers[name]))
     dump_tofile()
     log("update server_name=[" .. name .. "] server node list success.")
 end
@@ -177,26 +142,26 @@ local function load_from_local()
         log("dump file not exist,load config fail.")
         return
     end
-    local d ,err= file:read("*a")
+    local d, err = file:read("*a")
     if err then
-        log("read data from dump file fail. error:"..err)
+        log("read data from dump file fail. error:" .. err)
         file:close()
         return
     end
-    log("load config data from local, data:"..d)
-    local data = cjson_safe.decode(d)
-    _M.data = data
+    log("load config data from local, data:" .. d)
+    local nodes = cjson_safe.decode(d)
+    servers = nodes
     file:close()
 end
 
 local function init_servers()
-    local s_url = "/v2/keys" .. _M.conf.etcd_path .. "?recursive=true"
+    local s_url = "/v2/keys" .. _M.conf.etcd_path
     local data_json = query_etcd_data(s_url)
     if data_json then
         --for each server list
         for n, node in pairs(data_json.node.nodes) do
-            local _,end_index=string.find(node.key,_M.conf.etcd_path,1,true)
-            local server_name = string.sub(node.key,end_index+1,-1)
+            local _, end_index = string.find(node.key, _M.conf.etcd_path, 1, true)
+            local server_name = string.sub(node.key, end_index + 1, -1)
             update_server(server_name)
         end
     else
@@ -204,178 +169,161 @@ local function init_servers()
     end
 end
 
+local function proccess_action(message)
+    log("INFO: recieve change: " .. message)
+
+    local change = cjson_safe.decode(message)
+    --error message
+    if change.errorCode then
+        log("message has errorCode="..change.errorCode.." skip this message")
+        return
+    end
+
+    local action = change.action
+    local strs = string.split(change.node.key,"/")
+    if change.node.dir then
+        --key style /servers/server-name
+        if table.getn(strs) ~= 2 then
+            log("illegal etcd path,etcd_path=" .. change.node.key)
+            return
+        end
+        local server_name = strs[2]
+        if action == "delete" then
+            servers[server_name] = nil
+        else
+            update_server(server_name)
+        end
+    else
+        --key style /servers/server-name/localhost:8080
+        if table.getn(strs) ~= 3 then
+            log("illegal etcd path,etcd_path=" .. change.node.key)
+            return
+        end
+        update_server(strs[2])
+    end
+    dump_tofile()
+end
+
+local function do_watch()
+    local etcd_watch_http_connect
+    --get a effective connection
+    for _, node in pairs(etcd_node_list) do
+        etcd_watch_http_connect = http.new()
+        etcd_watch_http_connect:set_timeout(60000)
+        local ok, err = etcd_watch_http_connect:connect(node.host, node.port)
+        if ok then
+            break
+        else
+            log("conn " .. node.host .. ":" .. node.port .. " fail. error=" .. err)
+            etcd_watch_http_connect = nil
+        end
+    end
+
+    --no connection available
+    if not etcd_watch_http_connect then
+        log("no available etcd nodes")
+        return
+    end
+
+    local url = "/v2/keys" .. _M.conf.etcd_path .. "?wait=true&recursive=true"
+    if _M.etcd_index then
+        url = url .. "&waitIndex=" .. (_M.etcd_index + 1)
+    end
+    local res, err = etcd_watch_http_connect:request({ path = url, method = "GET" })
+    if err then
+        return
+    end
+    local body, err = res:read_body()
+    etcd_watch_http_connect:close()
+    if err or res.status ~= 200 then
+        return
+    end
+
+    local etcd_index = res.headers['X-Etcd-Index'];
+    --store the max etcd index
+    if not _M.etcd_index or (etcd_index and _M.etcd_index < etcd_index) then
+        _M.etcd_index = etcd_index
+    end
+    if body then
+        proccess_action(body)
+    end
+end
 
 local function watch(premature)
+    log("watch start...")
     if premature then
         log("time work premature")
         return
     end
-
-    if ngx_worker_exiting() then
-        log("work exit")
-        return
-    end
-
-    local http_connect = http:new()
-    http_connect:set_timeout(60000)
-
-    --get a effective connection
-    for _, node in pairs(_M.etcd_node_list) do
-        local ok, err = http_connect:connect(node.host, node.port)
-        if ok then
-            break
+    while true do
+        if ngx_worker_exiting() then
+            log("work exit,stop watch etcd")
+            return
         end
+        do_watch()
     end
-    --todo no connection available?
-
-    local url = "/v2/keys" .. _M.conf.etcd_path .. "?wait=true&recursive=true"
-    local res, err = http_connect:request({ path = url, method = "GET" })
-    if not err then
-        local body, err = res:read_body()
-        http_connect:close()
-        --restart watch
-        ngx_timer_at(0,watch)
-        --        ngx_timer_at(0, watch)
-        if not err then
-            log("INFO: recieve change: " .. body)
-            local change = cjson_safe.decode(body)
-            if not change.errorCode then
-                local action = change.action
-                local key = change.node.key
-                if change.node.dir then
-                    --key style /servers/server-name
-                    local _, count = string.gsub(key, "/", "/")
-                    if count ~= 2 then
-                        log("illegal etcd path,etcd_path="..key)
-                        return
-                    end
-                    local _,end_index=string.find(key,_M.conf.etcd_path,1,true)
-                    local server_name = string.sub(key,end_index+1,-1)
-                    if action == "delete" then
-                        _M.data[server_name] = nil
-                    else
-                        update_server(server_name)
-                    end
-                else
-                    local _, count = string.gsub(key, "/", "/")
-                    if count ~= 3 then
-                        log("illegal etcd path,etcd_path="..key)
-                        return
-                    end
-                    local _,end_index=string.find(key,_M.conf.etcd_path,1,true)
-                    local server_path = string.sub(key,end_index+1,-1)
-                    local _,_,server_name,node = server_path:find("(.*)/(.*)")
-                    update_server(server_name)
-                end
-            end
-            dump_tofile()
-        end
-    else
-        ngx_timer_at(0, watch)
-    end
-    return
 end
+
 
 function _M.init(conf)
     -- Load the upstreams from file
     log("start init dyupstream env")
     --format etcd patch
-    conf.etcd_path = string.gsub(conf.etcd_path.."/","//", "/")
+    conf.etcd_path = string.gsub(conf.etcd_path .. "/", "//", "/")
     _M.conf = conf
     --init etcd cluster list
-    _M.etcd_node_list = {}
-    log(_M.conf.etcd_urls)
+    etcd_node_list = {}
+    servers = {}
     for _, url in pairs(string.split(_M.conf.etcd_urls, ",")) do
         local host, port = url:match("(.*):([0-9]+)")
         local node = {}
         node.host = host
         node.port = port
-        table.insert(_M.etcd_node_list, node)
+        table.insert(etcd_node_list, node)
     end
     --init server configs
-    ngx.timer.at(0, init_servers)
-    log("end init dyupstream env success")
-
+    ngx_timer_at(0, init_servers)
     -- Start the etcd watcher
     ngx_timer_at(0, watch)
+    log("end init dyupstream env success")
 end
 
 -- Round robin
 function _M.round_robin_server(name)
 
-    if not _M.data[name] then
+    if not servers[name] then
         return nil, "upstream not ready."
     end
-
-    local c = ngx.shared[_M.conf.dict]
+    local dict = ngx.shared[_M.conf.dict]
     local robin_key = name .. "_count"
-    local count, err = c:incr(robin_key, 1)
+    local count, err = dict:incr(robin_key, 1)
     if not count then --incr fail
         count = 1
-        local ok = c:set(robin_key, 1)
+        local ok = dict:set(robin_key, 1)
         if not ok then
-            return random_server(name)
+            return _M.random_server(name)
         end
     end
-    local index = count % #_M.data[name].servers+1
-    return _M.data[name].servers[index]
+    local index = count % #servers[name].nodes + 1
+    return servers[name].nodes[index]
 end
 
 function _M.random_server(name)
-    local server_count = _M.data[name].count
-    if not _M.data[name] or not server_count or server_count<1 then
+    local server_count = servers[name].count
+    if not servers[name] or not server_count or server_count < 1 then
         return nil, "upstream not ready."
     end
-    local server_count = _M.data[name].count
-    local index = math.random(1,server_count)
-    return _M.data[name].servers[index]
+    local server_count = servers[name].count
+    local index = math.random(1, server_count)
+    return servers[name].nodes[index]
 end
 
-function _M.round_robin_with_weight(name)
-    if not _M.ready or not _M.data[name] then
-        return nil, "upstream not ready."
+function _M.all_servers(name)
+    if servers[name] then
+        return servers[name].nodes
+    else
+        return {}
     end
+end
 
-    local peers = _M.data[name].servers
-    local total = 0
-    local pick = nil
-
-    for _, peer in pairs(peers) do
-
-        -- If no weight set, the default is 1.
-        if peer.weight == nil then
-            peer.weight = 1
-        end
-
-        if peer.current_weight == nil then
-            peer.current_weight = 0
-        end
-
-        if peer.weight == 0 then
-            goto continue
-        end
-
-        peer.current_weight = peer.current_weight + peer.weight
-        total = total + peer.weight
-
-        if pick == nil or pick.current_weight < peer.current_weight then
-            pick = peer
-        end
-
-        :: continue ::
-        end
-
-        pick.current_weight = pick.current_weight - total
-
-        return pick
-    end
-
-    function _M.all_servers(name)
-        if _M.data[name] then
-            return _M.data[name].servers
-        else
-            return nil
-        end
-    end
-
-    return _M
+return _M
